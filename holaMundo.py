9fc995e934ddbb8efac7e104dd40acb2c335b170
Italia Barrón <141947806+brizavda@users.noreply.github.com>
2023-11-21 09:11:04 -0600

    Create holaMundo.py

---
+++ holaMundo.py
@@ -0,0 +1,8 @@
+def saludar(nombre, matricula):
+    print(f"Hola Mundo, {nombre}!")
+    print(f"Tu número de matrícula es: {matricula}")
+
+if __name__ == "__main__":
+    nombre = "Italia Zoe Barron Reyes"
+    matricula = 2223028502
+    saludar(nombre, matricula)
